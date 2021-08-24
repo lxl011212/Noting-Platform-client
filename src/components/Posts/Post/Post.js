a4e1acfef8b1ce9614708c6d59da705e74fd8646
Steven Liu <lxlsteven011212@gmail.com>
2021-08-24 15:42:27 -0400

    fixed typo

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -15,14 +15,14 @@ const Post = ({ post, setCurrentId }) => {
 
     return (
         <>
-        {(user?.result?.googleId === post?.userId) && (
+        {(user?.result?.googleId != post?.userId) && (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title}/>
             <div className={classes.overlay}>
                 <Typography variant="h6">{post.creator}</Typography>
                 <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
             </div>
-            {(user?.result?.googleId === post?.userId) && (
+            {(user?.result?.googleId != post?.userId) && (
                 <div className={classes.overlay2}>
                     <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(post._id)}><MoreHorizIcon fontSize="default" /></Button>
                 </div>
@@ -31,7 +31,7 @@ const Post = ({ post, setCurrentId }) => {
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
-            {(user?.result?.googleId === post?.userId) && (
+            {(user?.result?.googleId != post?.userId) && (
                 <CardActions className={classes.cardActions}>
                     <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
                 </CardActions>
