85940fb3bbc9403457f0128203cbb7ffe9cab221
Steven Liu <lxlsteven011212@gmail.com>
2021-08-24 13:55:36 -0400

    added useeffect in posts

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Grid, CircularProgress } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
@@ -7,10 +7,15 @@ import useStyles from './styles';
 const Posts = ({setCurrentId}) => {
     const unfilteredPosts = useSelector((state) => state.posts);
     const classes = useStyles();
-    const user = JSON.parse(localStorage.getItem('profile'));
-    const posts = unfilteredPosts.filter((post) => 
-        post?.userId !== user?.result?.googleId
-    )
+    let user = JSON.parse(localStorage.getItem('profile'));
+    let posts = [];
+    
+    useEffect(() => {
+        user = JSON.parse(localStorage.getItem('profile'));
+        posts = unfilteredPosts.filter(post => 
+            post?.userId !== user?.result?.googleId
+        )
+    }, [user]);
 
     return (
         !user?.result?.googleId ? <CircularProgress /> : (
